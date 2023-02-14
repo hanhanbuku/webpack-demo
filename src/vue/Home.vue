6cebf563cf30015769c1432b9c6ce18e5cd57a3a
wangyuhan123 <1070553788@qq.com>
2023-02-14 21:31:22 +0800

    feat: 首次提交
    (cherry picked from commit 6adc7a62645c0bca29c3952b48e911573c226a84)

---
+++ src/vue/Home.vue
@@ -0,0 +1,21 @@
+<template>
+<div>{{msg}}</div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "Home",
+  setup(){
+    const msg = ref('Home test')
+    return{
+      msg
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
